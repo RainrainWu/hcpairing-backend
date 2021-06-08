fba9b79b1baa4f536ed042c6dcb8b35b13c42db7
RainrainWu <s0958334772@gmail.com>
2021-06-08 16:14:33 +0800

    feat(main): patch entrypoint

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/RainrainWu/hcpairing"
 )
 
 func main() {
 
 	hcpairing.DBConn.Start()
-	// hcpairing.DBConn.AppendRecord("california", []string{"Cough", "Itchy Skin"})
-	for _, item := range hcpairing.DBConn.GetRecordsByState("california") {
-		fmt.Println(item)
-	}
-
 	server := hcpairing.NewServer()
 	server.Start()
 }
